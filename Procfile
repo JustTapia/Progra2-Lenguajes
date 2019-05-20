1cee6574cc829dd3b498a991519d6606331bc386
JustTapia <38672129+JustTapia@users.noreply.github.com>
2019-05-20 14:17:25 -0600

    Aplicación actualizada

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python apiRecetas.py
+web: gunicorn apiRecetas:app
--- apiRecetas.py
+++ apiRecetas.py
@@ -1,53 +1,130 @@
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request, make_response, abort
 import jwt
 import datetime
+import os
+import psycopg2
 from functools import wraps
+from cryptography.fernet import Fernet
+import json
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "Prueba12345"
+app.config['SECRET_KEY'] = "Secret Key"
+llave_cifra = b'pRmgMa8T0INjEAfksaq2aafzoZXEuwKI7wDe4c1F8AY='
 
-def token_required(f):
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+def toJSON(lista, nombreElemento): #Crea una variable en formato json de una lista, el nombre del elemento es el nombre que se le da en una vatiable
+	datos= []
+
+	for url in lista:
+		temp = {nombreElemento: url}
+		datos.append(temp)
+
+	return datos
+
+def token_required(f): #Comprueba que el token sea válido
 	@wraps(f)
 	def decorated(*args, **kwargs):
 		token = request.args.get('token')
 
 		if not token:
-			return jsonify({'mesage' : 'No hay Token'}),403
+			return abort(401)
 
 		try:
 			data = jwt.decode(token, app.config['SECRET_KEY'])
 		except:
-			return jsonify({'mesage' : 'Token invalido'}),403
+			return abort(401)
 
 
 		return f(*args, **kwargs)
 	return decorated
 
-@app.route('/get_recetas')
+@app.route('/get_recetas', methods=['GET']) #/get_recetas?token=blalblasadfa
 @token_required
 def getRecetas():
-	return jsonify({'mesage' : 'Recetas detras de Token'})
+	recetas = ["h","o","l","a","m","u","n","d","o",",","b","u","e","n","a","s",",","s","u","s","a","n","i","t","a","!","!"]
+	recetasJSON = {"Nombres" : recetas}
+	return jsonify(recetasJSON)
 
-@app.route('/show_receta')
+@app.route('/buscar_recetas', methods=['GET']) #/buscar_recetas?cBusqueda=Nombre&strBusqueda=Pollo&token=blalblasadfa
+@token_required
+def buscarRecetas():
+
+	recetas = ["h","o","l","a","m","u","n","d","o",",","b","u","e","n","a","s",",","s","u","s","a","n","i","t","a","!","!"]
+	recetasJSON = {"Nombres" : recetas}
+	return jsonify(recetasJSON)
+
+
+@app.route('/show_receta', methods=['GET']) #/show_receta?nombreReceta=Hola&token=blalblasadfa
 @token_required
 def showReceta():
-	return jsonify({'mesage' : 'Contenido de receta con Token'})
+	receta = ["Nombre","Tipo",["ing1","ing2","ing3"],["url1","url2"]]
 
-@app.route('/create_receta')
+	recetaJSON = {}
+	recetaJSON['Nombre'] = receta[0]
+	recetaJSON['Tipo'] = receta[1]
+	recetaJSON["Ingredientes"] = receta[2]
+	recetaJSON["Imagenes"] = receta[3]
+
+	return jsonify(recetaJSON)
+
+@app.route('/create_receta', methods=['POST']) #/create_receta, no está listo
 @token_required
 def createReceta():
 	return jsonify({'mesage' : 'Receta a crear con Token'})
 
 
-@app.route('/login')
+def auth_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		correo = request.args.get('correo')
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM CUENTAS WHERE CUENTAS.correo = '"+correo+"'") #Pide a la base de datos el usuario que comparta el correo
+		res = cursor.fetchone()
+		print(res)
+		if (res == []):
+			return abort("El usuario o la contraseña son incorrectos")
+		else: 
+			cipher_suite = Fernet(llave_cifra)
+			contrasena = res[1].encode()
+			contrasena = cipher_suite.decrypt(contrasena) #Descifrar
+			contrasena = contrasena.decode()
+
+			if (contrasena != request.args.get('contrasena')):
+				return abort("El usuario o la contraseña son incorrectos")
+		cursor.close()
+		return f(*args, **kwargs)
+	return decorated
+
+@app.route('/login', methods=['GET']) #/login?correo=correofalso@gmail.com&contrasena=contrasena
+@auth_required
 def login():
-	auth= request.authorization
+	correo = 	request.args.get('correo')
+	token = jwt.encode({'user': correo, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+	return jsonify({'token': token.decode('UTF-8')})
 
-	if auth and auth.password == "password":
-		token = jwt.encode({'user': auth.username, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+@app.route('/singUp',methods=['POST']) # /singUp?correo=correofalso@gmail.com&contrasena=helado123
+def singUp():
+	correo = request.args.get('correo')
+	contrasena = request.args.get('contrasena')
 
-		return jsonify({'token': token.decode('UTF-8')})
-	return make_response('Could not verify!', 401, {'WWW-Authenticate' : 'Basic realm="Login Requerido"'})
+	
+	try:
+
+		cipher_suite = Fernet(llave_cifra)
+		contrasena = cipher_suite.encrypt(contrasena.encode())   #Para cifrar 
+		contrasena = contrasena.decode()# convertir el binario a texto
+
+		cursor = conn.cursor()
+		cursor.execute("INSERT INTO cuentas VALUES (%s, %s)",(correo,contrasena)) #Intenta agregar el nuevo usuario a la base de datos
+		conn.commit()
+		cursor.close()
+		return jsonify({'message': 'El usuario ha sido registrado'})
+	except: #Falla si el correo ya existe en la base de datos
+		return jsonify({'message': 'El usuario ya está registrado'})
+
 
 if __name__ == '__main__':
     app.run()
